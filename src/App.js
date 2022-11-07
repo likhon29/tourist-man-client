c9cd26100f296385dd16a5c830968dbc457ef14b
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2022-11-07 21:53:33 +0600

    authprovider and router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
-
+import { Toaster } from "react-hot-toast";
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import { router } from "./Routes/Router/Router";
 function App() {
   return (
     <div>
-     
+      <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
     </div>
   );
 }
--- src/Context/AuthProvider/AuthProvider.js
+++ src/Context/AuthProvider/AuthProvider.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { createContext, useState } from "react";
 
-const AuthProvider = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import { getAuth } from "firebase/auth";
+import app from "../../Firebase/Firebase.Config";
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    const authInfo = {
+        user
+    }
+    return <AuthContext.Provider value={authInfo}>
+      {children}
+  </AuthContext.Provider>;
 };
 
 export default AuthProvider;
