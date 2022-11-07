d4f3f12341069ebd87b2fe35f88e52cfd63754be
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2022-11-07 21:31:11 +0600

    Folder structure done

---
+++ src/Firebase/Firebase.Config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-X7Yo9mJdVLsnnhrRGdKcFklFFS4baMA",
+  authDomain: "tourist-man-a4780.firebaseapp.com",
+  projectId: "tourist-man-a4780",
+  storageBucket: "tourist-man-a4780.appspot.com",
+  messagingSenderId: "318900230500",
+  appId: "1:318900230500:web:507d522d8faded73a20271"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
