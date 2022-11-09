29c09c299caba245aa632bff2f68034a5212bbd2
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2022-11-09 12:04:50 +0600

    firebase environment variable added

--- src/Firebase/Firebase.Config.js
+++ src/Firebase/Firebase.Config.js
@@ -5,12 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC-X7Yo9mJdVLsnnhrRGdKcFklFFS4baMA",
-  authDomain: "tourist-man-a4780.firebaseapp.com",
-  projectId: "tourist-man-a4780",
-  storageBucket: "tourist-man-a4780.appspot.com",
-  messagingSenderId: "318900230500",
-  appId: "1:318900230500:web:507d522d8faded73a20271"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  //   apiKey: "AIzaSyC-X7Yo9mJdVLsnnhrRGdKcFklFFS4baMA",
+  //   authDomain: "tourist-man-a4780.firebaseapp.com",
+  //   projectId: "tourist-man-a4780",
+  //   storageBucket: "tourist-man-a4780.appspot.com",
+  //   messagingSenderId: "318900230500",
+  //   appId: "1:318900230500:web:507d522d8faded73a20271"
 };
 
 // Initialize Firebase
